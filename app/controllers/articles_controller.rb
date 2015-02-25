a62e2fa0b95dcdeac51f57a98010ecaf02321b7c
Manish Arora <arora.manish3@tcs.com>
2015-02-25 10:48:11 +0530

    New Hash Notation used Used Where clause in place of find clause removed comments and pint statements

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,43 +4,34 @@ class ArticlesController < ApplicationController
 	end
  # method for saving new article posted in database
 	def create
-		# p "i am comming here"
-		# @article = Article.new(article_params)
-		# p ">>>>>>>>>>>"
-		# p article_params
-		# p @article
-		# if @article.save
-		#  	redirect_to @article
-		# else 
-		# 	render 'new'
-		# end
 		  @article = current_user.articles.create(article_params)
-		  redirect_to @article
-		  p params
-		 # p @user
-		 # @article = @user.articles.create
-		 # p ">>>>>>>>>>>>"
-		 # p @article 
-		 # p "<<<<<<<<<<<<<<"
-		 # redirect_to @article
+      respond_to do |format|
+      if @article.save
+      	format.html { redirect_to @article, notice: 'Article was successfully created.' }
+      	format.js   {}
+      	format.json { render json: @article, status: :created, location: @article }
+      else
+    		format.html { render action: "new" }
+      	format.json { render json: @article.errors, status: :unprocessable_entity }
+    	end
+  end
 
 	end
  #method to populate the existing article in update view
 	def edit
-  	@article = Article.find(params[:id])
+  	@article = Article.where(id: params[:id]).first
 	end
  # method to display the posted article
 	def show
-		@article = Article.find(params[:id])
-		p @article
+		@article = Article.where(id: params[:id]).first
 	end
 
 	def index
-     @articles= Article.paginate(:page => params[:page], :per_page => 5)
+     @articles= Article.paginate(page: params[:page], per_page: 5)
   end
 #method for editing the existing article
   def update
-    @article = Article.find(params[:id])
+    @article = Article.where(id: params[:id]).first
  
     if @article.user_id===current_user.id
        @article.update(article_params)
@@ -52,11 +43,11 @@ class ArticlesController < ApplicationController
   end
 #method to delete any existing article 
   def destroy
-  @article = Article.find(params[:id])
+  @article = Article.where(id: params[:id]).first
     p @article.user_id
     p current_user.id
   	if @article.user_id===current_user.id
-     	@article.destroy
+    @article.destroy
      	redirect_to articles_path
     else 
     	 redirect_to article_path, :alert => "You are not authorised to delete this post!"
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,21 +3,16 @@ class CommentsController < ApplicationController
 
 # method to add a comment on an article
   def create
-    @article = Article.find(params[:article_id])
-
+    @article = Article.where(id: params[:article_id]).first
     @comment =@article.comments.create(comment_params)
     @comment.Commentor_Id=current_user.id
-    p @comment.Commentor_Id
-    p current_user.id
-    p @comment
     @comment.save
     redirect_to article_path(@article)
   end
 #method to delete a comment on article
   def destroy
-     @article = Article.find(params[:article_id])
-     @comment = @article.comments.find(params[:id])
-
+     @article = Article.where(id: params[:article_id]).first
+     @comment = @article.comments.where(id :params[:id]).first
      if @comment.Commentor_Id===current_user.id
         @comment.destroy
         redirect_to article_path(@article)
