02b4c0622a38c6d70a847c4dadfcb958569fb83b
ManishArora93 <manisharora454@gmail.com>
2018-09-28 14:50:04 +0200

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,54 +4,53 @@ class ArticlesController < ApplicationController
 	end
  # method for saving new article posted in database
 	def create
-		  @article = current_user.articles.create(article_params)
-      respond_to do |format|
-      if @article.save
-      	format.html { redirect_to @article, notice: 'Article was successfully created.' }
-      	format.js   {}
-      	format.json { render json: @article, status: :created, location: @article }
-      else
-    		format.html { render action: "new" }
-      	format.json { render json: @article.errors, status: :unprocessable_entity }
-    	end
-  end
-
+	  @article = current_user.articles.create(article_params)
+	  respond_to do |format|
+	    if @article.save
+              format.html { redirect_to @article, notice: 'Article was successfully created.' }
+	      format.js   {}
+	      format.json { render json: @article, status: :created, location: @article }
+	    else
+	      format.html { render action: "new" }
+	      format.json { render json: @article.errors, status: :unprocessable_entity }
+	    end
+     end
 	end
- #method to populate the existing article in update view
+        #method to populate the existing article in update view
 	def edit
   	@article = Article.where(id: params[:id]).first
 	end
- # method to display the posted article
+	
+        # method to display the posted article
 	def show
-		@article = Article.where(id: params[:id]).first
+     @article = Article.where(id: params[:id]).first
 	end
 
 	def index
-     @articles= Article.paginate(page: params[:page], per_page: 5)
+    @articles= Article.paginate(page: params[:page], per_page: 5)
   end
-#method for editing the existing article
-  def update
-    @article = Article.where(id: params[:id]).first
- 
-    if @article.user_id===current_user.id
-       @article.update(article_params)
-       redirect_to @article
-    else
-       redirect_to article_path, :alert => "You are not authorised to update this post!"
+	
+       #method for editing the existing article
+ def update
+	 @article = Article.where(id: params[:id]).first
 
-    end
-  end
-#method to delete any existing article 
+	 if @article.user_id===current_user.id
+		 @article.update(article_params)
+		 redirect_to @article
+	 else
+		 redirect_to article_path, :alert => "You are not authorised to update this post!"
+	 end
+ end
+	
+      #method to delete any existing article 
   def destroy
-  @article = Article.where(id: params[:id]).first
-    p @article.user_id
-    p current_user.id
-  	if @article.user_id===current_user.id
-    @article.destroy
-     	redirect_to articles_path
-    else 
-    	 redirect_to article_path, :alert => "You are not authorised to delete this post!"
-    end
+		@article = Article.where(id: params[:id]).first
+		if @article.user_id===current_user.id
+			@article.destroy
+		  redirect_to articles_path
+	  else 
+		  redirect_to article_path, :alert => "You are not authorised to delete this post!"
+		end
   end
 
 	private
